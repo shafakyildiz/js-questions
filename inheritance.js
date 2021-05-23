460252d4b34fc3c4eb13a3b19b7fee8aea6d30a6
Şafak YILDIZ <shafakyildiz@gmail.com>
2021-05-24 00:19:03 +0300

    change inheritance

--- inheritance.js
+++ inheritance.js
@@ -1,1 +1,8 @@
-inheritance.js
+function Person(firstName, lastName) {
+    this.FirstName = firstName || "unknown";
+    this.LastName = lastName || "unknown";
+};
+
+Person.prototype.getFullName = function () {
+    return this.FirstName + " " + this.LastName;
+}
