33c60fc5407cf1b3b1a21f9366c018551ff6b9ed
Şafak YILDIZ <shafakyildiz@gmail.com>
2021-05-24 00:26:24 +0300

    create files

--- inheritance.js
+++ inheritance.js
@@ -6,3 +6,14 @@ function Person(firstName, lastName) {
 Person.prototype.getFullName = function () {
     return this.FirstName + " " + this.LastName;
 }
+
+function Student(firstName, lastName, schoolName, grade)
+{
+    Person.call(this, firstName, lastName);
+
+    this.SchoolName = schoolName || "unknown";
+    this.Grade = grade || 0;
+}
+//Student.prototype = Person.prototype;
+Student.prototype = new Person();
+Student.prototype.constructor = Student;
