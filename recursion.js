53c21f67a624e9a6c6e36a674102a1c65ca2f431
Şafak YILDIZ <shafakyildiz@gmail.com>
2021-05-25 02:40:08 +0300

    edit recursion

--- recursion.js
+++ recursion.js
@@ -1,9 +1,12 @@
 function Factorial(n) {
-    if (n === 1) {
-        return n;
-    } else {
-        return n * Factorial(n - 1);
-    }
+  if (n === 1 || n === 0) {
+    return 1;
+  } else {
+    return n * Factorial(n - 1);
+  }
 }
 
 Factorial(3); //6
+
+console.log(Factorial(4)); // 24
+console.log(Factorial(0)); // 1
