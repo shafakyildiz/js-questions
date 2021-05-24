57f2bbc54a7a9b406cceb7f75ebdcf114088ec49
Şafak YILDIZ <shafakyildiz@gmail.com>
2021-05-24 16:24:42 +0300

    edit inheritance.js

--- inheritance.js
+++ inheritance.js
@@ -1,9 +1,12 @@
 function User() {
-    console.log("User class...");
-    this.getName = () => {
-        return "Safak Yildiz";
-    };
-    
+  console.log("User class...");
+  this.getName = (name) => {
+    return "Hello " + name;
+  };
 }
 
 let user = new User();
+console.log(user.getName("Safak"));
+
+let user2 = new User();
+console.log(user.getName("Oguz"));
